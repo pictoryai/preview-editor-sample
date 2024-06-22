441157538aece99739e4be3e0daa94aec82ff318
shpals <shailendra>
2024-06-21 23:07:47 -0700

    added render params editor

--- src/components/EditorContainer/index.jsx
+++ src/components/EditorContainer/index.jsx
@@ -1,30 +1,60 @@
-import React, { Fragment, useState } from "react";
-import API from '../../services/API';
-import useErrorHandler from "../../utils/errorHandler";
-import Editor from "../Editor";
-import Error from "../Error";
-import Loader from "../Loader";
-import { Grid } from "@mui/material";
+import React, { useEffect, useState, useRef } from 'react'
+import API from '../../services/API'
+import useErrorHandler from '../../utils/errorHandler'
+import Editor from '../Editor'
+import Error from '../Error'
+import Loader from '../Loader'
+import { Grid } from '@mui/material'
+import storyboard from '../../data/storyboard.json'
+import JSONEditor from 'jsoneditor'
+import 'jsoneditor/dist/jsoneditor.min.css'
+import { v4 as uuid } from 'uuid'
 
 const EditorContainer = () => {
-  const [showEditor, setShowEditor] = useState(false);
-  const { error, showError, handleClose } = useErrorHandler();
-  const [editorUrl, setEditorUrl] = useState(null);
-  const [showLoader, setShowLoader] = useState(false);
+  const editorContainerRef = useRef(null)
+  const [jsonEditor, setJsonEditor] = useState(null)
+  const [renderParams, setRenderParams] = useState(null)
+
+  useEffect(() => {
+    if (editorContainerRef.current.children.length === 0) {
+      const options = {}
+      const editor = new JSONEditor(editorContainerRef.current, {
+        onChangeText: onPreviewJsonChange
+      })
+      editor.set(storyboard.renderParams)
+      setJsonEditor(editor)
+    }
+  }, [])
+
+  const onPreviewJsonChange = jsonText => {
+    let renderParams = JSON.parse(jsonText)
+    if (renderParams.scenes && renderParams.scenes.length > 0) {
+      for (let scene of renderParams.scenes) {
+        if (
+          scene.background &&
+          scene.background.src &&
+          scene.background.src.length > 0
+        ) {
+          for (let src of scene.background.src) {
+            src.resource_id = uuid()
+          }
+        }
+      }
+    }
+    setRenderParams(renderParams)
+  }
 
   return (
-    <Fragment>
-      <Loader show={showLoader}></Loader>
-      <Error open={error !== null} handleClose={handleClose} message={error} />
-      <Grid container spacing={2}>
-        <Grid item xs={6}>
-        </Grid>
-        <Grid item xs={6}>
-          <Editor onError={showError} editorUrl={editorUrl}></Editor>
-        </Grid>
+    <Grid container spacing={2}>
+      <Grid item xs={6} ref={editorContainerRef}></Grid>
+      <Grid item xs={6}>
+        <Editor
+          renderParams={renderParams}
+          editorUrl={storyboard.preview}
+        ></Editor>
       </Grid>
-    </Fragment>
-  );
-};
+    </Grid>
+  )
+}
 
-export default EditorContainer;
+export default EditorContainer
--- src/components/Loader/index.jsx
+++ src/components/Loader/index.jsx
@@ -1,7 +1,26 @@
+import React, { useRef, useEffect } from 'react';
 import Box from "@mui/material/Box";
 import { CircularProgress } from "@mui/material";
 
+function getOffset(el) {
+    const rect = el.getBoundingClientRect();
+    return {
+        left: rect.left + window.scrollX,
+        top: rect.top + window.scrollY,
+        height: rect.height,
+        width: rect.width
+    };
+}
+
 export default ({ show }) => {
+    const loadingOverlayContainer = useRef(null);
+    useEffect(() => {
+        if (loadingOverlayContainer.current && loadingOverlayContainer.current.parentNode) {
+            let parentOffSet = getOffset(loadingOverlayContainer.current.parentNode);
+            loadingOverlayContainer.current.style.height = `${parentOffSet.height}px`;
+            loadingOverlayContainer.current.style.width = `${parentOffSet.width}px`;
+        }
+    })
     return (
         <>
             {
@@ -19,7 +38,7 @@ export default ({ show }) => {
                             zIndex: 100,
                             background: "gray",
                             opacity: 0.5
-                        }}>
+                        }} ref={loadingOverlayContainer}>
                         <CircularProgress size={40} sx={{ color: "rgba(18, 18, 18, 0.6)" }} />
                     </Box>
                     :
